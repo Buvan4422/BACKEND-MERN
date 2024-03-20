806b860c5226e3c0832e2ede142195183b6c5f6d
Buvan4422 <buvansingh05@gmail.com>
2024-03-20 15:15:13 +0530

    client.http,userapi changes made mongodb server co connected

--- APIs/user-api.js
+++ APIs/user-api.js
@@ -4,41 +4,49 @@ const Userapp = express.Router();
 Userapp.use(express.json());
 
 //create API(routes)
-
+let userCollection;
+Userapp.use((req, res, next) => {
+  userCollection = req.app.get('userCollection');
+  next();
+});
 //route to read users
 Userapp.get('/', (req, res) => {
   res.redirect('/users');
 });
 
 Userapp.get('/users', async (req, res) => {
-  const userCollection = req.app.get('userCollection');
-
   let usersList = await userCollection.find().toArray();
 
   res.send({ message: 'your data', payload: usersList });
 });
 
 Userapp.post('/user', async (req, res) => {
-  const userCollection = req.app.get('userCollection');
+  //const userCollection = req.app.get('userCollection');
   let newUser = req.body;
 
   await userCollection.insertOne(newUser);
   res.send({ message: 'New user  created' });
 });
 
-Userapp.put('/user', (req, res) => {
-  let uUp = req.body;
-  let uid = users.findIndex((ele) => ele.id === uUp.id);
-  users.splice(uid, 1, uUp);
-  console.log(users);
-  res.send({ message: 'User updated' });
+Userapp.put('/user', async (req, res) => {
+  //const usersCollection = req.app.get('userCollection');
+
+  let modifiedUser = req.body;
+  await userCollection.findOneAndUpdate(
+    { userId: modifiedUser.userId },
+    { $set: { ...modifiedUser } },
+    { returnDocument: 'after' }
+  );
+  res.send('User modified');
 });
 
-Userapp.delete('/user/:id', (req, res) => {
-  let uid = Number(req.params.id);
-  let idx = users.findIndex((ele) => ele.id === uid);
-  users.splice(idx, 1);
+Userapp.delete('/user/:userId', async (req, res) => {
+  //get userCollection obj
+  //const usersCollection = req.app.get('userCollection');
+  //get id from params
+  let id = Number(req.params.userId);
+  //delete user from DB
+  await userCollection.deleteOne({ userId: id });
   res.send({ message: 'User deleted' });
 });
-
 module.exports = Userapp;
